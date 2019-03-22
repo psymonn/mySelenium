c4508fa9f7c2122d29f0daf5aa8c1778df68c6ff
psymonn <40018954+psymonn@users.noreply.github.com>
2019-03-22 19:08:33 +1100

    update for parallel testing

---
+++ RunPester.ps1
@@ -0,0 +1,18 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [String]$browser,
+    [String]$source
+ )
+ 
+ Try {
+ 
+     Write-verbose "Executing Pester tests"
+                          
+     Invoke-Pester -verbose -OutputFile ${browser}.xml  -OutputFormat NUnitXml -EnableExit -Script @{ Path = $source; Parameters = @{browser = $browser;};}
+        
+ }
+ Catch {
+    Exit 1
+ }
+ 
