4b295df0543dee634b5de72411196188ed99f223
psymonn <40018954+psymonn@users.noreply.github.com>
2019-03-22 19:26:32 +1100

    Update RunPester.ps1

--- RunPester.ps1
+++ RunPester.ps1
@@ -1,7 +1,7 @@
 ﻿[CmdletBinding()]
 param (
     [Parameter(Mandatory=$true)]
-    [String]$browser,
+    [String]$IISSite,
     [String]$source,
     [String]$outFile
  )
@@ -10,7 +10,7 @@ param (
  
      Write-verbose "Executing Pester tests"
                           
-     Invoke-Pester -verbose -OutputFile $outFile -OutputFormat NUnitXml -EnableExit -Script @{ Path = $source; Parameters = @{browser = $browser;};}
+     Invoke-Pester -verbose -OutputFile $outFile -OutputFormat NUnitXml -EnableExit -Script @{ Path = $source; Parameters = @{IISSite = $IISSite;};}
         
  }
  Catch {
